7cd24239666213c163d6ae423c5f888b39c3b318
kelseylw1822 <kelseylw1822@gmail.com>
2025-01-15 16:38:13 -0500

    Initial Commit

---
+++ Debugging/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Debugging
+//
+//  Created by Kelsey Wessel on 1/15/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
