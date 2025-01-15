e238937fef6fdaf5d283e6c7556d64b7dea8c5df
kelseylw1822 <kelseylw1822@gmail.com>
2025-01-15 16:43:54 -0500

    finished two of the chapter two bugs

---
+++ Debugging/Ch02-02-bug.cpp
@@ -0,0 +1,18 @@
+// My Name: Kelsey Wessel
+// My Class: CPSC 1021
+// Date: 1/15/2025
+// Desc: Prints out a value.
+// Time: Less than 5 minutes.
+
+// This program can't find its variable.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int value = 100;
+    cout << value;
+    return 0;
+}
+
+
